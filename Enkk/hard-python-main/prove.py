353f896c89486713160aa1cd5c398bb19a391a0d
andrea-prestini <andrea.prestini@gmail.com>
2024-04-24 10:04:38 +0200

    add: file prove.py per fare esercizi casuali

--- Enkk/hard-python-main/prove.py
+++ Enkk/hard-python-main/prove.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 import time
 
 
@@ -6,24 +7,5 @@ with open("deadge.json") as f:
     data = json.load(f)
 
 
-def take_anno_mese(f_anno, f_mese):
-    for anno, anno_vals in data.items():
-        if anno == str(f_anno):
-            for mese, mese_vals in anno_vals.items():
-                if mese == f_mese.capitalize():
-                    count_morti_mese = 0
-                    for giorno, giorno_vals in mese_vals.items():
-                        print("-" * 50)
-                        print(f"anno: {anno}")
-                        print(f"mese: {mese}")
-                        print(f"giorno: {giorno}")
-
-                        for persona in giorno_vals:
-                            print(persona["name"])
-                        print("morti", len(giorno_vals))
-                        count_morti_mese += len(giorno_vals)
-    time.sleep(0.2)
-    print("morti mese:", count_morti_mese)
-
-
-take_anno_mese(1992, "january")
+df = pd.DataFrame(data)
+print(df["1992"].value_counts())
